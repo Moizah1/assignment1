2ea357214b1521625078932f378f0b1d400027ac
Moizah1 <moizahnaeem12@gmail.com>
2020-11-29 23:08:45 +0500

    ifelse

---
+++ ifelse/ifst.java
@@ -0,0 +1,11 @@
+class ifst { 
+    public static void main(String args[]) 
+    {  
+        int i = 4;  
+        if (i == 4) { 
+            i++; 
+        } 
+        // Executed by default 
+        System.out.println("i = " + i); 
+    } 
+} 
